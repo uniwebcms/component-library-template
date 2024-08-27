d99d8bb0dc428f50ea180f2de94434b5174f54e9
XIANG ZUO <35638284+xiangzuo951218@users.noreply.github.com>
2024-08-27 13:58:47 -0400

    modify Block section component

--- src/WebsiteModules/components/Block/index.js
+++ src/WebsiteModules/components/Block/index.js
@@ -1,41 +1,21 @@
 import React from 'react';
-import { FiPhone } from 'react-icons/fi';
-import { MdOutlineMailOutline } from 'react-icons/md';
-import { FiMapPin } from 'react-icons/fi';
+import { SafeHtml } from '@uniwebcms/module-sdk';
 
 export default (props) => {
-    // a example of block element with some sample content
+    const { block } = props;
+    const { main } = block;
+
+    const { title = '', subtitle = '', pretitle = '' } = main.header || {};
+    const paragraphs = main.body?.paragraphs || [];
+
     return (
-        <section className='flex items-center justify-center mt-20'>
-            <div className='border rounded-lg flex justify-between min-h-[180px] min-w-[360px] bg-primary'>
-                <div className='w-5/12 flex items-center justify-center px-1'>
-                    <img
-                        className='w-avatar h-avatar object-cover rounded-avatar'
-                        src='https://images.unsplash.com/photo-1544348817-5f2cf14b88c8?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'></img>
-                </div>
-                <div className='w-7/12 py-4 pr-4'>
-                    <p className='text-lg font-bold tracking-wide mb-3'>Jason Smith</p>
-                    <div className='flex items-center space-x-2 mt-2'>
-                        <div className='w-6 h-6 flex items-center justify-center rounded-full bg-white p-1 border'>
-                            <FiPhone className='text-gray-500 w-full h-full' />
-                        </div>
-                        <p className='text-sm font-medium tracking-wide'>+1 234 567 890</p>
-                    </div>
-                    <div className='flex items-center space-x-2 mt-2'>
-                        <div className='w-6 h-6 flex items-center justify-center rounded-full bg-white p-1 border'>
-                            <MdOutlineMailOutline className='text-gray-500 w-full h-full' />
-                        </div>
-                        <p className='text-sm font-medium tracking-wide'>
-                            <a href='mailto:example.com'>Example.com</a>
-                        </p>
-                    </div>
-                    <div className='flex items-center space-x-2 mt-2'>
-                        <div className='w-6 h-6 flex items-center justify-center rounded-full bg-white p-1 border'>
-                            <FiMapPin className='text-gray-500 w-full h-full' />
-                        </div>
-                        <p className='text-sm font-medium tracking-wide'>123 Maple Street</p>
-                    </div>
-                </div>
+        <section className='flex max-w-7xl mx-auto my-20'>
+            <p className='text-gray-700 font-medium'>Content of the section:</p>
+            <div className='mt-12 px-6 py-4 border rounded-lg min-w-full'>
+                {pretitle && <p className='text-xl font-medium'>{pretitle}</p>}
+                {title && <h2 className='text-4xl font-bold mt-1'>{title}</h2>}
+                {subtitle && <p className='text-lg mt-0.5'>{subtitle}</p>}
+                {paragraphs.length ? <SafeHtml value={paragraphs} className='mt-4 text-gray-800' /> : null}
             </div>
         </section>
     );
