f508c4e6df1d75e7cf60dad2f675b33b52145afb
Uniweb CMS <127919770+uniwebcms@users.noreply.github.com>
2024-09-13 15:22:45 -0400

    Update index.js

--- src/WebsiteModules/src/index.js
+++ src/WebsiteModules/src/index.js
@@ -1,8 +1,17 @@
-// import CSS
+// Import custom CSS
 import './index.css';
 
-// import all dynamically exported components
+// Import the components from the auto-generated dynamicExports.js file
 import * as dynamicExports from './dynamicExports';
 
-// export all dynamically exported components
+// Export selectable components under the module's "default" property
 export default dynamicExports;
+
+// Export site-level components and settings under the module's "site" property
+// Example:
+
+// export const site = {
+//     Layout: MyLayoutComponent       // take control of the site's layout
+//     props: { disableTheming: true } // eg, disable auto theming
+//     report: { ... }                 // render as a document (eg, Word/Excel/JSON)
+// };
