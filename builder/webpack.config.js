e04b93817324b95bf41554622e2e7db8851b88b5
Diego Macrini <macrini@proximify.ca>
2024-10-10 14:35:02 -0400

    Change to match new builder

--- builder/webpack.config.js
+++ builder/webpack.config.js
@@ -1,11 +1,7 @@
-const { getConfigData } = require('@uniwebcms/module-builder');
+const { getWebpackConfig } = require('@uniwebcms/module-builder');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: '../.env.dev' });
 dotenv.config({ path: '../.env' });
 
-module.exports = (_, argv) => {
-    const { config } = getConfigData(argv, __dirname);
-
-    return config;
-};
+module.exports = (_, argv) => getWebpackConfig(argv, __dirname);
